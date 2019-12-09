94507c8df87a7afc181e7e0471d2ddd68ce7feee
Vitali <Vitali_Sharstuk@epam.com>
2019-12-09 05:52:34 +0300

    home page

---
+++ src/main/java/com/epam/vsharstuk/config/ServletConfig.java
@@ -0,0 +1,26 @@
+package com.epam.vsharstuk.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("com.epam.vsharstuk")
+@EnableWebMvc
+public class ServletConfig extends WebMvcConfigurerAdapter {
+
+    @Bean(name = "internalResourceViewResolver")
+    public InternalResourceViewResolver getInternalViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
---
+++ src/main/java/com/epam/vsharstuk/config/WebAppInitializer.java
@@ -0,0 +1,28 @@
+package com.epam.vsharstuk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        ConfigurableWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("WEB-INF/spring-context.xml");
+        ServletRegistration.Dynamic dispather = container.addServlet("spring-web",
+                new DispatcherServlet(appContext));
+        /*MultipartConfigElement multipartConfigElement = new MultipartConfigElement(null, 15000000, 15000000, 0);
+        dispather.setMultipartConfig(multipartConfigElement);*/
+        dispather.setLoadOnStartup(1);
+        dispather.addMapping("/");
+
+
+    }
+}
